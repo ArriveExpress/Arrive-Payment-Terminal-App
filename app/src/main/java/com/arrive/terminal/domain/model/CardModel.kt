778e2eb95d27c8572608f28d36fb1e1bbd34f703
Duddy Rosenberg <63026090+DuddyRosenberg@users.noreply.github.com>
2025-10-30 18:02:53 -0400

    KAN-31 Add manual cc fields to card model

--- app/src/main/java/com/arrive/terminal/domain/model/CardModel.kt
+++ app/src/main/java/com/arrive/terminal/domain/model/CardModel.kt
@@ -1,7 +1,10 @@
 package com.arrive.terminal.domain.model;
 
 data class CardModel(
+    val isManualEntry: Boolean = false,
     val number: String,
     val cardExpireMonth: String,
     val cardExpireYear: String,
+    val cvc: String? = null,
+    val zipCode: String? = null,
 )
