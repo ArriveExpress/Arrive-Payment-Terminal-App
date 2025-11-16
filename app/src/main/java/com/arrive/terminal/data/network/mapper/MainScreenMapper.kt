59e213e1289ee13061e0ca3c5f07af145b945631
Duddy Rosenberg <63026090+DuddyRosenberg@users.noreply.github.com>
2025-11-16 15:16:52 -0500

    KAN-64 Add todayAdSchedules to main screen mapper

--- app/src/main/java/com/arrive/terminal/data/network/mapper/MainScreenMapper.kt
+++ app/src/main/java/com/arrive/terminal/data/network/mapper/MainScreenMapper.kt
@@ -2,6 +2,7 @@ package com.arrive.terminal.data.network.mapper;
 
 import com.arrive.terminal.core.data.mappers.BaseMapper
 import com.arrive.terminal.data.network.response.MainScreenNT
+import com.arrive.terminal.domain.model.AdScheduleModel
 import com.arrive.terminal.domain.model.FlaggedTripModel
 import com.arrive.terminal.domain.model.MainScreenModel
 import com.arrive.terminal.domain.model.RideModel
@@ -41,6 +42,17 @@ class MainScreenMapper(model: MainScreenNT) : BaseMapper<MainScreenNT>(model) {
                 } else {
                     null
                 }
+            },
+            todaysAdSchedules = model.todaysAdSchedules.orEmpty().map { adScheduleNT ->
+                AdScheduleModel(
+                    multiply = adScheduleNT.multiply,
+                    ad = adScheduleNT.ad?.let { adNT ->
+                        AdScheduleModel.AdModel(
+                            id = adNT.id,
+                            imageUrl = adNT.imageUrl
+                        )
+                    }
+                )
             }
         )
     }
